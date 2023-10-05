70768a3ae9ae981cb737247a12ae5bed4c2ca5ef
Edward Ryzewski <Eryzhevskij@mail.ru>
2023-10-05 16:12:16 +0300

    listed mocked data with restaurants

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { restaurants } from './materials/mock'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<div>
+ <h1>{restaurants[0].name}</h1>
+  <h2>Menu</h2>
+  <ul>
+    <li>{restaurants[0].menu[0].name}</li>
+    <li>{restaurants[0].menu[1].name}</li>
+    <li>{restaurants[0].menu[2].name}</li>
+  </ul>
+  <h3>Reviews</h3>
+  <ul>
+    <li>{restaurants[0].reviews[0].text}</li>
+    <li>{restaurants[0].reviews[1].text}</li>
+  </ul>
+
+  <h1>{restaurants[1].name}</h1>
+  <h2>Menu</h2>
+  <ul>
+    <li>{restaurants[1].menu[0].name}</li>
+    <li>{restaurants[1].menu[1].name}</li>
+  </ul>
+  <h3>Reviews</h3>
+  <ul>
+    <li>{restaurants[1].reviews[0].text}</li>
+    <li>{restaurants[1].reviews[1].text}</li>
+    <li>{restaurants[1].reviews[2].text}</li>
+  </ul>
+
+  <h1>{restaurants[2].name}</h1>
+  <h2>Menu</h2>
+  <ul>
+    <li>{restaurants[2].menu[0].name}</li>
+    <li>{restaurants[2].menu[1].name}</li>
+    <li>{restaurants[2].menu[2].name}</li>
+  </ul>
+  <h3>Reviews</h3>
+  <ul>
+    <li>{restaurants[2].reviews[0].text}</li>
+  </ul>
+
+  <h1>{restaurants[3].name}</h1>
+  <h2>Menu</h2>
+  <ul>
+    <li>{restaurants[3].menu[0].name}</li>
+    <li>{restaurants[3].menu[1].name}</li>
+  </ul>
+  <h3>Reviews</h3>
+  <ul>
+    <li>{restaurants[3].reviews[0].text}</li>
+    <li>{restaurants[3].reviews[1].text}</li>
+  </ul>
+</div>
 )
